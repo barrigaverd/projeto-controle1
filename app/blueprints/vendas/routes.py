5df090dc413686d880000c0bd6e200e63bd788e3
Sérgio Ugoline Junior <sergiouine@gmail.com>
2025-10-15 13:55:59 -0300

    criado pagina vendas e seu html inicial

--- app/blueprints/vendas/routes.py
+++ app/blueprints/vendas/routes.py
@@ -4,4 +4,12 @@ vendas_bp = Blueprint('vendas', __name__, template_folder='templates')
 
 @vendas_bp.route('/')
 def index():
-    return render_template('vendas/index.html', page_title='Venda de Produtos')
+    resumo = {
+        'quantidade': 16,
+        'receita': 291,
+        'recebido': 122,
+        'a_receber': 169
+    }
+    vendas = [
+        {'codigo': 96089, 'Cliente': 'Clientes barraca', 'Categoria': 'lapis', 'Produto': 'Lápis Infinito', 'Complemento': '', 'Qtdade': 1, 'Vendido': 8, 'Recebido': 8, 'À receber': 0, 'Data compra': "20/04/2025", 'Baixa Estoque?': "Sim", "Observações": ""}]
+    return render_template('vendas/index.html', page_title='Venda de Produtos', resumo=resumo, vendas=vendas)
